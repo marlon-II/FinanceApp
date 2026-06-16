61177be0b3c1014410f6be6cfec0d75ffe0b4320
marlon-II <marlonbartolit@gmail.com>
2026-06-16 07:52:03 -0300

    ReportController criada

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -3,8 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Expense;
+use App\Models\Income;
+use Barryvdh\DomPDF\Facade\Pdf;
+use Maatwebsite\Excel\Facades\Excel;
 
 class ReportController extends Controller
 {
-    //
+    public function index(){
+        $expenses = Expense::where('user_id', auth()->id())
+        ->with('category')
+        ->get();
+
+        $incomes = Income::where('user_id', auth()->id())
+        ->get();
+
+        return view('reports.index', compact('expenses', 'incomes'));
+    }
+
+    public function exportPdf(){
+        $expenses = Expense::where('user_id', auth()->id())
+        ->with('category')
+        ->get();
+
+        $incomes = Income::where('user_id', auth()->id())
+        ->get();
+
+        $pdf = Pdf::loadView('reports.pdf', compact('expenses', 'incomes'));
+        return $pdf->download('DespesasGanhos.pdf');
+    }
+
+    public function exportExcel()
+    {
+    return Excel::download(new ClassExport(), 'PlanilhaDespesasGanhos.xlsx');
+    }
 }
